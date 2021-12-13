8a71c908a66d8eeba28ff0d0e6ee2237cdfa6a3e
Kyle Scharnhorst <kyle.scharnhorst@gmail.com>
2021-12-13 04:31:50 -0800

    Complete task 4.
    Fix code style issues for the code style attachment.

--- task4/coding_style_modified.c
+++ task4/coding_style_modified.c
@@ -1,43 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0
+
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <asm/delay.h>
+#include <linux/delay.h>
 #include <linux/slab.h>
 
-int do_work( int * my_int, int retval ) {
-int x;
-int y=*my_int;
-int z;
-
-for(x=0;x< * my_int;++x) {
-	udelay(10);
-}
-
-if (y < 10 )
-	// That was a long sleep, tell userspace about it
-	printk("We slept a long time!");
-
-z = x * y;
-
-return z;
-
-
-}
-
-int
-my_init (void)
+int do_work(int *my_int, int retval)
 {
-int x = 10;
+	int x;
+	int y = *my_int;
+	int z;
 
-x = do_work(&x, x);
+	for (x = 0; x < *my_int; ++x)
+		udelay(10);
 
-return x;
+	if (y < 10)
+		// That was a long sleep, tell userspace about it
+		pr_info("We slept a long time!");
 
+	z = x * y;
 
+	return z;
 }
 
-void my_exit( void )
+int my_init(void)
 {
-return;
+	int x = 10;
+
+	x = do_work(&x, x);
+
+	return x;
+}
+
+void my_exit(void)
+{
+	return 0;
 }
 
 module_init(my_init);
